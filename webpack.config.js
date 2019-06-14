3d4d5f7a3d817791e6442973aa19c6b0d53d53bc
kciter <kciter@naver.com>
2019-06-14 17:41:23 +0900

    security warning fix [CVE-2018-14732]

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,47 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-var name = 'qart';
-var plugins = [], outputFile;
-var env = process.env.WEBPACK_ENV;
-var target = 'umd';
-
-if (env === 'build') {
-	plugins.push(new webpack.optimize.UglifyJsPlugin({
-	    compress: {
-	        warnings: false
-        }
-    }));
-	outputFile = name + '.min.js';
-} else {
-	outputFile = name + '.js';
-}
+const name = "qart";
 
 module.exports = {
-	entry: {
-		qart: './src/qart.js'
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: outputFile,
-		library: name,
-    	libraryTarget: target,
-		publicPath: '../dist/'
-	},
-	module: {
-		loaders: [
-	        {
-	            test: /\.js$/,
-	            loader: 'babel',
-	            exclude: /node_modules/,
-	            query: {
-	                cacheDirectory: true,
-	                presets: ['es2015']
-	            },
-	            include: [
-	                path.resolve(__dirname, '../')
-	            ]
-	        }
-		],
-	},
-	plugins: plugins,
-	devtool: 'cheap-module-source-map',
-	devServer: {
-		overlay: true
-	}
-}
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       cache: true,
+  //       parallel: true,
+  //       uglifyOptions: {
+  //         compress: false,
+  //         ecma: 6,
+  //         mangle: true
+  //       },
+  //       sourceMap: true
+  //     })
+  //   ]
+  // },
+  entry: {
+    qart: "./src/qart.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "qart.js",
+    library: name,
+    libraryTarget: "umd",
+    publicPath: "../dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, "/src")]
+      }
+    ]
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    overlay: true
+  },
+  mode: process.env.WEBPACK_ENV
+};
